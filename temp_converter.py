d67b8052fec0930b6285a1b43d8cbb4b6fa2117e
shruthi <sbshruthi2007@gmail.com>
2026-06-17 15:21:03 +0530

    Day 3 task modified

--- temp_converter.py
+++ temp_converter.py
@@ -1,15 +1,14 @@
-
+from utlis import celsius_to_fahrenheit
+from utlis import fahrenheit_to_celsius
 print("Temperature Converter")
 print("1.Celsius To Fahrenheit")
 print("2.Fahrenheit To Celsius")
 choice=int(input("Enter the Choice(1 or 2):"))
 if choice==1:
     celsius=float(input("Enter the temperature in celsius:"))
-    fahrenheit=(celsius*9/5)+32
-    print(f"{celsius}C={fahrenheit:.2f}F")
+    print(celsius_to_fahrenheit(celsius))
 elif choice==2:
     fahrenheit=float(input("Enter the temperature in fahrenheit:"))
-    celsius=(fahrenheit-32)*5/9
-    print(f"{fahrenheit}F={celsius:.2f}C")
+    print(fahrenheit_to_celsius(fahrenheit))
 else:
     print("Invalid choice")
